3be2afb0c53e84a0ff504be6ea831e9fc792d946
Aleksey Dalinin <root1@192.168.1.74>
2025-09-13 15:00:24 +0300

    Fix CSP to allow inline scripts
    - Add 'unsafe-inline' to scriptSrc in Content Security Policy
    - This allows inline JavaScript initialization in HTML
    - Required for object initialization script to work properly

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
             styleSrc: [
                 "'self'", 
                 "'unsafe-inline'",
